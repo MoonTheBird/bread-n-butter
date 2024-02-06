fa0ad1413a72f2177e1447f19b6a2aae7d0a6f85
Moon <brandibrooks4@gmail.com>
2024-02-06 15:47:34 -0700

    initial commit

---
+++ breadnbutter/breadnbuttermodsystems.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace BreadNButter
+{
+    public class BreadNButterModSystem : ModSystem
+    {
+        
+    }
+}
